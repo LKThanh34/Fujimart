26021dcf5cf2a13677881a1ca2704c4875bac13b
LeKimThanh <lekimthanhx34@gmail.com>
2024-08-27 10:34:45 +0700

    final commit

---
+++ Ecommerce/Controllers/HangHoaController.cs
@@ -0,0 +1,52 @@
+﻿using Ecommerce.Data;
+using Ecommerce.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ecommerce.Controllers
+{
+    public class HangHoaController : Controller
+    {
+        private readonly EshopContext db;
+
+        public HangHoaController(EshopContext context) {
+            db = context;
+
+        }
+        public IActionResult Index(int? loai)
+        {
+            var hangHoas = db.HangHoas.AsQueryable();
+            if (loai.HasValue)
+            {
+                hangHoas = hangHoas.Where(p => p.MaLoai == loai.Value);
+            }
+            var result = hangHoas.Select(p => new HangHoaVM{
+                MaHh = p.MaHh,
+                TenHH = p.TenHh,
+                DonGia = p.DonGia ?? 0,
+                Hinh = p.Hinh ?? "",
+                MoTaNgan = p.MoTaDonVi ?? "" ,
+                TenLoai = p.MaLoaiNavigation.TenLoai
+            });
+            return View(result);
+        }
+        public IActionResult Search(string? query)
+        {
+            var hangHoas = db.HangHoas.AsQueryable();
+            if (query != null)
+            {
+                hangHoas = hangHoas.Where(p => p.TenHh.Contains(query));
+            }
+            var result = hangHoas.Select(p => new HangHoaVM
+            {
+                MaHh = p.MaHh,
+                TenHH = p.TenHh,
+                DonGia = p.DonGia ?? 0,
+                Hinh = p.Hinh ?? "",
+                MoTaNgan = p.MoTaDonVi ?? "",
+                TenLoai = p.MaLoaiNavigation.TenLoai
+            });
+            return View(result);
+
+        }
+    }
+}
